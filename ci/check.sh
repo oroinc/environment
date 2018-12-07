a2dc887df8218ab26af082c70117c71c01f55045
Hryhorii Hrebiniuk <hhrebiniuk@oroinc.com>
2018-12-07 13:08:53 +0200

    BAP-17930: Broken inline editor template (#20990)

--- ci/check.sh
+++ ci/check.sh
@@ -86,7 +86,7 @@ php_code_style)
 behat)
   echo "Defining strategy for Behat Tests..."
   set +e
-  files=$(grep -e "^application/" -e "^package/" -e "^environment/" -e "^environment/" -e "^Jenkinsfile" -e "^.jenkins" "$DIR_DIFF/$FILE_DIFF" | grep -v -E "\.(msi|ods|psd|bat|gif|gitignore|gitkeep|html|jpg|jpeg|md|mp4|png|py|rst|txt|gliffy)$")
+  files=$(grep -e "^application/" -e "^package/" -e "^environment/" -e "^environment/" -e "^Jenkinsfile" -e "^.jenkins" "$DIR_DIFF/$FILE_DIFF" | grep -v -E "\.(msi|ods|psd|bat|gif|gitignore|gitkeep|jpg|jpeg|md|mp4|png|py|rst|txt|gliffy)$")
   set -e
   if [[ "${files}" ]]; then
     echo "Changes were detected"
@@ -97,7 +97,7 @@ behat)
 behat_wiring)
   echo "Defining strategy for behat_wiring Tests..."
   set +e
-  files=$(grep -e "^application/" -e "^package/" -e "^environment/" -e "^Jenkinsfile" -e "^.jenkins" "$DIR_DIFF/$FILE_DIFF" | grep -v -E "\.(msi|ods|psd|bat|gif|gitignore|gitkeep|html|jpg|jpeg|md|mp4|png|py|rst|txt|gliffy)$")
+  files=$(grep -e "^application/" -e "^package/" -e "^environment/" -e "^Jenkinsfile" -e "^.jenkins" "$DIR_DIFF/$FILE_DIFF" | grep -v -E "\.(msi|ods|psd|bat|gif|gitignore|gitkeep|jpg|jpeg|md|mp4|png|py|rst|txt|gliffy)$")
   set -e
   if [[ "${files}" ]]; then
     echo "Changes were detected"
